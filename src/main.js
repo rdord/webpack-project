219ed53220cbd4d4d863f22ce62bc870af941754
Roland Đorđević <roland.djordjevic@ineor.si>
2019-12-12 20:02:28 +0100

    add babel env present

--- src/main.js
+++ src/main.js
@@ -1,7 +1,11 @@
-require("./main.css");
-require("./images/link.jpg");
-require("./index.html");
+require('./main.css');
+require('./images/link.jpg');
+require('./index.html');
 
-var a = () => {
-  console.log("Hello from the future!");
+var a = async args => {
+  const { a, b } = args;
+  await console.log('Hello from the future!');
+  console.log('Done!');
 };
+
+a({ a: 1, b: 2 });
